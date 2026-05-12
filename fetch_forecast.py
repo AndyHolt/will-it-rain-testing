a8cc8e967701ddf809e0f7b63241a3c37e1c6659
Andy Holt <andrew.holt@hotmail.co.uk>
2026-05-12 14:00:37 +0100

    combined dataframe/csv output with all models

--- fetch_forecast.py
+++ fetch_forecast.py
@@ -1,3 +1,4 @@
+from math import comb
 import openmeteo_requests
 from openmeteo_sdk import Model
 import pandas as pd
@@ -30,36 +31,37 @@ openmeteo = openmeteo_requests.Client(session=retry_session)
 
 # N.B. order of variables in hourly must match below for correct assignment
 url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+variables = [
+    "temperature_2m",
+    "relative_humidity_2m",
+    "apparent_temperature",
+    "precipitation_probability",
+    "precipitation",
+    "rain",
+    "showers",
+    "cloud_cover",
+    "wind_speed_10m",
+    "wind_direction_10m",
+]
+models = [
+    "best_match",
+    "ecmwf_ifs",
+    # best_match for specified location is identical to ukmo_uk... except that it also includes preciptation probability  column
+    # "ukmo_uk_deterministic_2km",
+]
 params = {
     "latitude": settings.LATITUDE,
     "longitude": settings.LONGITUDE,
     "start_date": "2026-04-25",
     "end_date": "2026-05-09",
-    "hourly": [
-        "temperature_2m",
-        "relative_humidity_2m",
-        "apparent_temperature",
-        "precipitation_probability",
-        "precipitation",
-        "rain",
-        "showers",
-        "cloud_cover",
-        "wind_speed_10m",
-        "wind_direction_10m",
-    ],
-    "models": [
-        "best_match",
-        "ecmwf_ifs",
-        # best_match for specified location is identical to ukmo_uk... except that it also includes preciptation probability  column
-        # "ukmo_uk_deterministic_2km",
-    ],
+    "hourly": variables,
+    "models": models,
 }
 responses = openmeteo.weather_api(url, params=params)
 
-model_dataframes = {}
+combined_dataframe = None
 
 for response in responses:
-
     lat_direction = "N" if response.Latitude() > 0 else "S"
     long_direction = "E" if response.Longitude() > 0 else "W"
 
@@ -71,20 +73,8 @@ for response in responses:
     model_name = model_to_name(model_id)
     print(f"Model: {model_name}")
 
-
     # process hourly data
     hourly = response.Hourly()
-
-    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
-    hourly_apparent_temperature = hourly.Variables(2).ValuesAsNumpy()
-    hourly_precipitation_probability = hourly.Variables(3).ValuesAsNumpy()
-    hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
-    hourly_rain = hourly.Variables(5).ValuesAsNumpy()
-    hourly_showers = hourly.Variables(6).ValuesAsNumpy()
-    hourly_cloud_cover = hourly.Variables(7).ValuesAsNumpy()
-    hourly_wind_speed_10m = hourly.Variables(8).ValuesAsNumpy()
-    hourly_wind_direction_10m = hourly.Variables(9).ValuesAsNumpy()
 
     hourly_data = {
         "date": pd.date_range(
@@ -95,19 +85,16 @@ for response in responses:
         )
     }
 
-    hourly_data["temperature_2m"] = hourly_temperature_2m
-    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
-    hourly_data["apparent_temperature"] = hourly_apparent_temperature
-    hourly_data["precipitation_probability"] = hourly_precipitation_probability
-    hourly_data["precipitation"] = hourly_precipitation
-    hourly_data["rain"] = hourly_rain
-    hourly_data["showers"] = hourly_showers
-    hourly_data["cloud_cover"] = hourly_cloud_cover
-    hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
-    hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+    for i, variable_name in enumerate(variables):
+        hourly_data[f"{model_name}__{variable_name}"] = hourly.Variables(i).ValuesAsNumpy()
 
     hourly_dataframe = pd.DataFrame(data = hourly_data)
-    print("\nHourly data\n", hourly_dataframe)
 
-    model_dataframes[model_id] = hourly_dataframe
-    # hourly_dataframe.to_csv("forecast.csv", encoding='utf-8')
+    if combined_dataframe is None:
+        combined_dataframe = hourly_dataframe
+    else:
+        combined_dataframe = combined_dataframe.merge(hourly_dataframe, on="date", how="outer")
+
+print("\nHourly data\n", combined_dataframe)
+
+combined_dataframe.to_csv("forecast.csv", index=False, encoding="utf-8")
